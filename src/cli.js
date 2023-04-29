cc67b318e510623f72135696a5e688370fed6ef9
Alexander Romanov <raibinger@gmail.com>
2023-04-29 13:48:20 +0300

    edit cli

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,4 @@
-import readlineSync from 'readline-sync';
+import readlineSync from 'readline-sync'; //это библиотека с Github позволяет работать в консоли и ожидать ответа пользователя
 
 export const userName = () => {
     const name = readlineSync.question('May I have your name? ');
