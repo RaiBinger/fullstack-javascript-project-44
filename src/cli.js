7b102631332ce5bb94f341e68654b0ea698b972f
Alexander Romanov <raibinger@gmail.com>
2023-05-17 09:28:16 +0300

    edit cli.js add normalize name

--- src/cli.js
+++ src/cli.js
@@ -3,8 +3,9 @@ import readlineSync from 'readline-sync'; // это библиотека с Gith
 const greeting = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-  return name;
+  const normalizeName = `${name[0].toUpperCase()}${name.slice(1).toLowerCase()}`;
+  console.log(`Hello, ${normalizeName}!`);
+  return normalizeName;
 };
 
 export default greeting;
